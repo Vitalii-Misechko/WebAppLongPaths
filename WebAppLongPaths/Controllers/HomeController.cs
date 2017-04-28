019694583fef12fa5debf6199aded8b715c8258b
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2017-04-28 22:27:26 +0300

    Initial commit

---
+++ WebAppLongPaths/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppLongPaths.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			return View();
+		}
+
+		public ActionResult About() {
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact() {
+			ViewBag.Message = "Your contact page.";
+
+			string dir = "";
+			foreach( var num in new[] {1,2,3,4,5,6,7,8,9,0} ) {
+				dir = dir + "/" + new String( num.ToString()[0], 30 );
+			}
+			
+			using( StreamWriter _testData = new StreamWriter( Server.MapPath( $"~/Temp{dir}/data.txt" ), true ) ) {
+				_testData.WriteLine( "Hello" ); // Write the file.
+			}
+
+			return View();
+		}
+	}
+}
---
+++ WebAppLongPaths/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebAppLongPaths
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+		protected void Application_Start()
+        {
+			System.AppContext.SetSwitch( "Switch.System.IO.BlockLongPaths", false );
+
+			System.AppContext.SetSwitch( "Switch.System.IO.UseLegacyPathHandling", false );
+
+			bool stillUsingLegacyPaths;
+			AppContext.TryGetSwitch( "Switch.System.IO.UseLegacyPathHandling", out stillUsingLegacyPaths );
+
+			bool stillBlockLongPaths;
+			AppContext.TryGetSwitch( "Switch.System.IO.UseLegacyPathHandling", out stillBlockLongPaths );
+
+			if( stillUsingLegacyPaths || stillBlockLongPaths ) {
+
+				throw new Exception( "Testing will fail if we are using legacy path handling." );
+
+			};
+
+
+			AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
