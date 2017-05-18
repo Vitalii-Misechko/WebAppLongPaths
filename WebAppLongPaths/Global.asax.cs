7eae9ec3924530e9fdcb317965283471ec55e789
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2017-05-18 15:38:45 +0300

    Long paths works fine

--- WebAppLongPaths/Global.asax.cs
+++ WebAppLongPaths/Global.asax.cs
@@ -12,23 +12,6 @@ namespace WebAppLongPaths
     {
 		protected void Application_Start()
         {
-			System.AppContext.SetSwitch( "Switch.System.IO.BlockLongPaths", false );
-
-			System.AppContext.SetSwitch( "Switch.System.IO.UseLegacyPathHandling", false );
-
-			bool stillUsingLegacyPaths;
-			AppContext.TryGetSwitch( "Switch.System.IO.UseLegacyPathHandling", out stillUsingLegacyPaths );
-
-			bool stillBlockLongPaths;
-			AppContext.TryGetSwitch( "Switch.System.IO.UseLegacyPathHandling", out stillBlockLongPaths );
-
-			if( stillUsingLegacyPaths || stillBlockLongPaths ) {
-
-				throw new Exception( "Testing will fail if we are using legacy path handling." );
-
-			};
-
-
 			AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
