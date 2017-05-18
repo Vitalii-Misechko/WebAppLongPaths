9c5b8a83a2978ccb35f5cc38e580ef43039d90f8
Vitalii Misechko <Vitalii.Misechko@d2l.com>
2017-05-18 15:29:27 +0300

    It works with long paths

--- WebAppLongPaths/Controllers/HomeController.cs
+++ WebAppLongPaths/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace WebAppLongPaths.Controllers {
 				dir = dir + "/" + new String( num.ToString()[0], 30 );
 			}
 
-			var fullDir = $"\\\\?\\c:/D2L/GIT/WebAppLongPaths/WebAppLongPaths/Temp{dir}/data.txt";
+			var fullDir = $"c:/D2L/GIT/WebAppLongPaths/WebAppLongPaths/Temp{dir}/";
 
 			Directory.CreateDirectory( fullDir );
 			
